fe39c890239ae4b7924a1775a90a65c4cf144309
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-25 19:01:05 -0600

    Take the input string and process as numbers but can't get the result printed on the webpage

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// addition
-
-// substraction
-
-// division
-
-// multiplication
-
 const textArea = document.getElementById("equation");
 textArea.value;
 
@@ -14,90 +6,47 @@ allClear.addEventListener("click", () => {
   textArea.value = "";
 });
 
-const num = document.querySelectorAll(".num");
-num.forEach((num) => {
+let = document.querySelectorAll(".num");
+let.forEach((num) => {
   num.addEventListener("click", () => {
     textArea.value += num.textContent;
   });
 });
 
-const op = document.querySelectorAll(".op");
-op.forEach((op) => {
+let = document.querySelectorAll(".op");
+let.forEach((op) => {
   op.addEventListener("click", () => {
     textArea.value += op.textContent;
   });
 });
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const operator = textArea.value.filter(
-  (operator) => !numbers.includes(operator)
-);
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let operants = /[+\-÷×]/g;
+let operator = function() {return textArea.value.split("").filter(num => !numbers.includes(num));}
 
-const splitting = textArea.value.split(/[+-÷×]/);
-const firstOp = +splitting[0];
-const secondOp = +splitting[1];
+let splitting = function() {return textArea.value.split(operants)};
+let firstOp = () => +splitting()[0];
+let secondOp = () => +splitting()[1];
 
 const equalTo = document.querySelector(".equal-to");
 equalTo.addEventListener("click", () => {
-  switch (operator) {
+  switch (operator()) {
     case "+":
-      textArea.value += firstOp + secondOp;
+      textArea.value = `${firstOp() + secondOp()}`;
       break;
     case "-":
-      textArea.value += firstOp - secondOp;
+      textArea.value = `${firstOp() - secondOp()}`;
       break;
     case "×":
-      textArea.value += firstOp * secondOp;
+      textArea.value = `${firstOp() * secondOp()}`;
       break;
     case "÷":
-      textArea.value += firstOp / secondOp;
+      textArea.value = secondOp() === 0 ? "Error" : `${firstOp() / secondOp()}`;
       break;
   }
 });
-/*
-//filter operator
-function getOperator() {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const operator = textArea.value.filter(
-    (operator) => !numbers.includes(operator)
-  );
-}
 
-//filter numbers
-function splitOperators() {
-  const splitting = textArea.value.split(/[+-÷×]/);
-  const firstOp = +splitting[0];
-  const secondOp = +splitting[1];
-}
-*/
 
-/*
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const operator = textArea.value.filter(
-  (operator) => !numbers.includes(operator)
-);
-
-const splitting = textArea.value.split(/[+-÷×]/);
-const firstOp = +splitting[0];
-const secondOp = +splitting[1];
-
-function operates(firstOp, secondOp, operator) {
-  switch (operator) {
-    case "+":
-      textArea.value += firstOp + secondOp;
-      break;
-    case "-":
-      textArea.value += firstOp - secondOp;
-      break;
-    case "×":
-      textArea.value += firstOp * secondOp;
-      break;
-    case "÷":
-      textArea.value += firstOp / secondOp;
-      break;
-  }
-}
-*/
 //equalTo loop the textArea string splitting by operator delimiting
 //store the value from the left side numbers and another from the right side numbers, including dot
 //convert them to numeric values
@@ -106,17 +55,3 @@ function operates(firstOp, secondOp, operator) {
 //run it using both stored values as parameters
 //if no operator is found, add input to textArea.value
 //add the result replacing the textArea.value and print the input above the result using a smaller font size
-
-/*
-const buttonsContainer = document.querySelector(".buttons");
-
-const buttons = buttonsContainer.querySelectorAll("button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.textContent !== "=" || button.textContent !== "AC") {
-      textArea.value += button.textContent;
-    }
-  });
-});
-*/
